7d773b178d407031b60f062d072747d2d9bb5113
firebolt182 <firebolt182@mail.ru>
2024-03-07 23:50:29 +0300

    3 этап - Добавил классы RunnerProm и RunnerTest. добавил туда  данные. извенил имена файлов сборки

---
+++ civil_registry/src/main/java/org/javaacademy/RunnerTest.java
@@ -0,0 +1,47 @@
+package org.javaacademy;
+
+import java.time.LocalDate;
+
+public class RunnerTest {
+    public static void main(String[] args) {
+        CivilRegistry civilRegistry = new CivilRegistry("TEST_ZAGS");
+        Citizen maria = new Citizen("Maria", "Ad", "Petrova",
+                Sex.FEMALE, FamilyType.NOT_MARRIED, null);
+        Citizen stepan = new Citizen("Stepan", "Ad", "Prokhorov",
+                Sex.MALE, FamilyType.NOT_MARRIED, null);
+        Citizen jane = new Citizen("Jane", "Ad", "Doe",
+                Sex.FEMALE, FamilyType.NOT_MARRIED, null);
+        Citizen jack = new Citizen("Jack", "Ad", "Dex",
+                Sex.MALE, FamilyType.MARRIED, jane);
+        jane.setMarriedWith(jack);
+        Citizen irina = new Citizen("Irina", "Ad", "Testova",
+                Sex.FEMALE, FamilyType.MARRIED, null);
+        Citizen mike = new Citizen("Mike", "Ad", "Ivanov",
+                Sex.MALE, FamilyType.NOT_MARRIED, irina);
+        mike.setMarriedWith(irina);
+        Citizen elena = new Citizen("elena", "Ad", "Bolshova",
+                Sex.FEMALE, FamilyType.NOT_MARRIED, null);
+        Citizen nikita = new Citizen("Nikita", "Ad", "Nikitin",
+                Sex.MALE, FamilyType.NOT_MARRIED, null);
+        Citizen son1 = new Citizen("Bill", "Ad", "Billy",
+                Sex.MALE, FamilyType.NOT_MARRIED, null);
+        Citizen son2 = new Citizen("Bob", "Ad", "Bills",
+                Sex.MALE, FamilyType.NOT_MARRIED, null);
+        Citizen son3 = new Citizen("Lilly", "Ad", "Billian",
+                Sex.FEMALE, FamilyType.NOT_MARRIED, null);
+        Citizen son4 = new Citizen("Jenny", "Ad", "Billenko",
+                Sex.FEMALE, FamilyType.NOT_MARRIED, null);
+        Citizen son5 = new Citizen("Katy", "Ad", "Billiard",
+                Sex.FEMALE, FamilyType.NOT_MARRIED, null);
+
+        civilRegistry.weddingRegistration(maria, stepan, LocalDate.of(2024, 3, 4));
+        civilRegistry.weddingRegistration(jane, jack, LocalDate.of(2024, 3, 4));
+        civilRegistry.divorceRegistration(irina, mike, LocalDate.of(2024, 3, 4));
+        civilRegistry.childBirth(son1, elena, nikita, LocalDate.of(2024, 3, 4));
+        civilRegistry.childBirth(son2, irina, mike, LocalDate.of(2024, 3, 4));
+        civilRegistry.childBirth(son3, jane, jack, LocalDate.of(2024, 3, 4));
+        civilRegistry.childBirth(son4, maria, stepan, LocalDate.of(2024, 3, 4));
+        civilRegistry.childBirth(son5, elena, mike, LocalDate.of(2024, 3, 4));
+        System.out.println(civilRegistry.makeStatistics(LocalDate.of(2024, 3, 4)));
+    }
+}
