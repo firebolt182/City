f5ca62c1fdb9e30d66d8ec7a64745f4165ed9b34
Lex-Smith <e.d.smith@mail.ru>
2024-03-11 15:34:56 +0300

    corrected Employee class

--- profession/src/main/java/org/javaacademy/Employee.java
+++ profession/src/main/java/org/javaacademy/Employee.java
@@ -1,13 +1,14 @@
 package org.javaacademy;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Employee extends Human{
     private int hourlyRate;
 
     public Employee(String name, String middleName, String lastName, Sex sex) {
         super(name, middleName, lastName, sex);
-    }
-
-    public void setHourlyRate(int hourlyRate) {
-        this.hourlyRate = hourlyRate;
     }
 }
